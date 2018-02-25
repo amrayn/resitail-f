b2a2de2636e747972649ec1e25401f97f6eb7992
Majid <mkhan3189@gmail.com>
2018-02-25 13:17:47 +1100

    update to use tail-browser as basis

--- index.js
+++ index.js
@@ -1,15 +1,136 @@
+//
+//  Copyright 2017-present Muflihun Labs
+//
+//  Author: @abumusamq
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
 "use strict";
 
 const fs = require('fs');
+const socket = require('socket.io');
+const Server = require('./lib/server.js');
 
 function resitailf (options) {
     this.config = options.config;
     this.serverInfo = options.serverInfo;
-}
 
-resitailf.prototype.send = (data) => {
-    console.log('resitail-f is not ready yet, please contribute to the project @ https://github.com/muflihun/resitail-f');
-    console.log(data);
+    const server = new Server(this.config);
+    this.io = server.io;
+    
+    this.connected_clients = [];
+
+    this.set_recent = (recent) => {
+        this.recent = recent;
+    }
+
+    this.send = (data) => {
+        const loggerId = data.logger_id || data.channel_name;
+        const clientId = data.client_id || data.channel_name;
+
+        for (var i = 0; i < this.connected_clients.length; ++i) {
+            if (this.connected_clients[i].ignore_loggers_list.indexOf(loggerId) === -1 &&
+                this.connected_clients[i].ignore_clients_list.indexOf(clientId) === -1) {
+                this.io.sockets.connected[this.connected_clients[i].socket].emit("data", data);
+            }
+        }
+    }
+
+    server.io.sockets.on('connection', (socket) => {
+        const _this = this;
+    
+        socket.on('client-ready', function() {
+            _this.connected_clients.push({
+                socket: socket.id,
+                ignore_clients_list: [],
+                ignore_loggers_list: [],
+            });
+            socket.emit("server-info", _this.serverInfo);
+
+            if (_this.recent) {
+                if (_this.recent.loggers) {
+                    _this.recent.loggers.forEach(element => {
+                        if (!includes(_this.config.loggers_ignore_list, element.channel_name)) {
+                            _this.send(element);
+                        }
+                    });
+                }
+                if (_this.recent.clients) {
+                    _this.recent.clients.forEach(element => {
+                        if (!includes(_this.config.clients_ignore_list, element.channel_name)) {
+                            _this.send(element);
+                        }
+                    });
+                }
+            }
+        });
+
+        socket.on('stop-client', function(data) {
+            for (var i = 0; i < _this.connected_clients.length; ++i) {
+                if (_this.connected_clients[i].socket === socket.id) {
+                    if (_this.connected_clients[i].ignore_clients_list.indexOf(data.id) === -1) {
+                        _this.connected_clients[i].ignore_clients_list.push(data.id);
+                    }
+                    break;
+                }
+            }
+        });
+
+        socket.on('stop-logger', function(data) {
+            for (var i = 0; i < _this.connected_clients.length; ++i) {
+                if (_this.connected_clients[i].socket === socket.id) {
+                    if (_this.connected_clients[i].ignore_loggers_list.indexOf(data.id) === -1) {
+                        _this.connected_clients[i].ignore_loggers_list.push(data.id);
+                    }
+                    break;
+                }
+            }
+        });
+
+        socket.on('start-client', function(data) {
+            for (var i = 0; i < _this.connected_clients.length; ++i) {
+                if (_this.connected_clients[i].socket === socket.id) {
+                    const idx = _this.connected_clients[i].ignore_clients_list.indexOf(data.id);
+                    if (idx !== -1) {
+                        _this.connected_clients[i].ignore_clients_list.splice(idx, 1);
+                    }
+                    break;
+                }
+            }
+        });
+
+        socket.on('start-logger', function(data) {
+            for (var i = 0; i < _this.connected_clients.length; ++i) {
+                if (_this.connected_clients[i].socket === socket.id) {
+                    const idx = _this.connected_clients[i].ignore_loggers_list.indexOf(data.id);
+                    if (idx !== -1) {
+                        _this.connected_clients[i].ignore_loggers_list.splice(idx, 1);
+                    }
+                    break;
+                }
+            }
+        });
+
+        socket.on('disconnect', function() {
+            for (var i = 0; i < _this.connected_clients.length; ++i) {
+                if (_this.connected_clients[i].socket === socket.id) {
+                    _this.connected_clients.splice(i, 1);
+                    break;
+                }
+            }
+        });
+    });
 }
 
 module.exports = (options) => new resitailf(options);
---
+++ web/scripts/app.js
@@ -0,0 +1,93 @@
+
+  var socket = io.connect('/');
+
+  $(document).ready(function() {
+      socket.emit("client-ready");
+  });
+  
+  $(document).on("click", "#side-bar>.clients input.client", function() {
+      if ($(this).is(":checked")) {
+          socket.emit("start-client", {
+              id: $(this).attr("name"),
+          });
+          $("." + $(this).attr("name") + "-loggers-list").find(".logger").removeProp('disabled');
+      } else {
+          $(".line[data-client=" + $(this).attr("name") + "]").remove();
+          $("." + $(this).attr("name") + "-loggers-list").find(".logger").prop('disabled', true);
+          socket.emit("stop-client", {
+              id: $(this).attr("name"),
+          });
+      }
+  });
+  
+  $(document).on("click", "#side-bar>.clients .loggers-list input.logger", function() {
+      if ($(this).is(":checked")) {
+          socket.emit("start-logger", {
+              id: $(this).attr("name"),
+          });
+      } else {
+          $(".line[data-logger=" + $(this).attr("name") + "]").remove();
+          socket.emit("stop-logger", {
+              id: $(this).attr("name"),
+          });
+      }
+  });
+
+  socket.on("server-info", function(serverInfo){
+      serverInfo.clients.forEach((client) => {
+          const clientId = client.client_id;
+          if ($("#side-bar").find(".client[id=chk-client-" + clientId + "]").length == 0) {
+              $("#side-bar>.clients").append($("<input>", {
+                  "type": "checkbox",
+                  "checked": true,
+                  "text": clientId,
+                  "class": "client",
+                  "id": "chk-client-" + clientId,
+                  "name": clientId,
+              }));
+              $("#side-bar>.clients").append($("<label>", {
+                  "for": "chk-client-" + clientId,
+                  "text": clientId,
+                  "class": "client-label",
+              }));
+              $("#side-bar>.clients").append("<div class='" + clientId + "-loggers-list loggers-list'></div>");
+              $("#side-bar>.clients").append("<br/>");
+          }
+          client.loggers.forEach((loggerId) => {
+              if ($("." + clientId + "-loggers-list").find(".logger[id=chk-logger-" + loggerId + "]").length == 0) {
+                  $("." + clientId + "-loggers-list").append($("<input>", {
+                      "type": "checkbox",
+                      "checked": true,
+                      "text": loggerId,
+                      "class": "logger",
+                      "id": "chk-logger-" + loggerId,
+                      "name": loggerId,
+                  }));
+
+                  $("." + clientId + "-loggers-list").append($("<label>", {
+                      "for": "chk-logger-" + loggerId,
+                      "text": loggerId,
+                      "class": "logger-label",
+                  }));
+                  $("." + clientId + "-loggers-list").append("<br/>");
+              }
+          });
+      });
+  });
+  
+  socket.on("data", function(data){
+      const loggerId = data.logger_id || data.channel_name;
+      const clientId = data.client_id || data.channel_name;
+      
+      const newLine = $("<div>", {
+          "text": data.line,
+          "class": "line " + 'evt-' + data.event_type + ' ' + ($("#chk-logger-" + loggerId).is(":checked") ? "" : "hidden-logger") + ($("#chk-client-" + clientId).is(":checked") ? "" : "hidden-client"),
+          "data-logger": loggerId,
+          "data-client": clientId,
+      });
+      $("#lines").append(newLine);
+      
+      if ($("#follow").is(":checked")) {
+          newLine[0].scrollIntoView(false);
+      }
+  });
